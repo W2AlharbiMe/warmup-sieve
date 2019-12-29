145d51e7124e2c375442ad5334624591433a852e
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-29 09:44:02 +0300

    prime numbers

---
+++ main.js
@@ -0,0 +1,19 @@
+const removed = [];
+const numbers = [...Array(121).keys()];
+numbers.splice(0, 2); // remove 0, 1
+
+for (let i = 2; i < numbers.length; i++) {
+  if (removed.includes(i)) continue;
+
+  for (let j = 2; j < numbers.length; j++) {
+    const prime = i * j;
+    if (prime > 121) break;
+    if (!removed.includes(prime)) {
+      removed.push(prime);
+      const index = numbers.indexOf(prime);
+      numbers.splice(index, 1);
+    }
+  }
+}
+
+console.log("Prime Numbers => ", numbers.join(", "));
